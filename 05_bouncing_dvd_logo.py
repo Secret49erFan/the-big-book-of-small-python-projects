214bad768c83a5423f7d29499e0b5de03d0db234
Alex Flores <alexontheiphone@gmail.com>
2025-12-25 20:54:38 -0800

    Update complete Bouncing DvD logo and repo README.

--- 05_bouncing_dvd_logo.py
+++ 05_bouncing_dvd_logo.py
@@ -24,6 +24,9 @@ WIDTH, HEIGHT = bext.size()
 # We can't print to the last column on Windows without it adding a
 # newline automatically, so reduce the width by one.
 WIDTH -= 1
+# Keep WIDTH odd so WIDTH - 3 stays even and matches logo X positions.
+if WIDTH % 2 == 0:
+    WIDTH -= 1
 
 NUMBER_OF_LOGOS = 5
 PAUSE_AMOUNT = 0.2
@@ -61,3 +64,90 @@ def main():
         for logo in logos: # Handle each logo in the logos list.
             # Erase the logo's current location.
             bext.goto(logo[X], logo[Y])
+            print('   ', end='')
+
+            original_direction = logo[DIR]
+
+            # See if the logo bounces off the corners.
+            if logo[X] == 0 and logo[Y] == 0:
+                logo[DIR] = DOWN_RIGHT
+                corner_bounces += 1
+            elif logo[X] == 0 and logo[Y] == HEIGHT - 1:
+                logo[DIR] = UP_RIGHT
+                corner_bounces += 1
+            elif logo[X] == WIDTH - 3 and logo[Y] == 0:
+                logo[DIR] = DOWN_LEFT
+                corner_bounces += 1
+            elif logo[X] == WIDTH - 3 and logo[Y] == HEIGHT - 1:
+                logo[DIR] = UP_LEFT
+                corner_bounces += 1
+            
+            # See if the logo bounces off the left edge.
+            elif logo[X] == 0 and logo[DIR] == UP_LEFT:
+                logo[DIR] = UP_RIGHT
+            elif logo[X] == 0 and logo[DIR] == DOWN_LEFT:
+                logo[DIR] = DOWN_RIGHT
+            
+            # See if the logo bounces off the right edge.
+            # (WIDTH - 3 because 'DVD' has 3 letters.)
+            elif logo[X] == WIDTH - 3 and logo[DIR] == UP_RIGHT:
+                logo[DIR] = UP_LEFT
+            elif logo[X] == WIDTH - 3 and logo[DIR] == DOWN_RIGHT:
+                logo[DIR] = DOWN_LEFT
+            
+            # See if the logo bounces off the top edge:
+            elif logo[Y] == 0 and logo[DIR] == UP_LEFT:
+                logo[DIR] = DOWN_LEFT
+            elif logo[Y] == 0 and logo[DIR] == UP_RIGHT:
+                logo[DIR] = DOWN_RIGHT
+
+            # See if the logo bounces off the bottom edge.
+            elif logo[Y] == HEIGHT - 1 and logo[DIR] == DOWN_LEFT:
+                logo[DIR] = UP_LEFT
+            elif logo[Y] == HEIGHT - 1 and logo[DIR] == DOWN_RIGHT:
+                logo[DIR] = UP_RIGHT
+            
+            if logo[DIR] != original_direction:
+                # Change color when the logo bounces.
+                logo[COLOR] = random.choice(COLORS)
+            
+            # Move the logo. (X move by 2 because the terminal
+            # characters are twice as tall as they are wide.)
+            if logo[DIR] == UP_RIGHT:
+                logo[X] += 2
+                logo[Y] -= 1
+            elif logo[DIR] == UP_LEFT:
+                logo[X] -= 2
+                logo[Y] -= 1
+            elif logo[DIR] == DOWN_RIGHT:
+                logo[X] += 2
+                logo[Y] += 1
+            elif logo[DIR] == DOWN_LEFT:
+                logo[X] -= 2
+                logo[Y] += 1
+            
+            # Display number of corner bounces.
+        bext.goto(5, 0)
+        bext.fg('white')
+        print(f'Corner bounces: {corner_bounces}', end='')
+
+        for logo in logos:
+            # Draw the logos a their new location.
+            bext.goto(logo[X], logo[Y])
+            bext.fg(logo[COLOR])
+            print('DvD', end='')
+            
+        bext.goto(0, 0)
+
+        sys.stdout.flush() # (Required for bext-using programs.)
+        time.sleep(PAUSE_AMOUNT)
+
+
+# If this program is run, instead of imporated, run the program.
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print()
+        print('Bouncing DvD logo, Alex Flores')
+        sys.exit() # When Ctrl-C is pressed, end the program.
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # the-big-book-of-small-python-projects
 This is where I'm stashing all the code snippets from the book. 
+
+## Bagels
+    -> Added some f-strings.
+    -> Changed variable names to snake_case.
+
+## Birthday Paradox
+    -> Added f-strings.
+    -> Added a variable for any birthday.
+    -> Changed displayed month from abbreviated to full text.
+    -> Changed variable names to snake_case.
+
+## Bitmap Message
+    -> Added f-strings.
+    -> Changed the bitmap used.
+    -> Changed variable names to snake_case.
+
+## Blackjack
+    -> Added f-strings.
+    -> Added Insurance.
+        + Bug: Ignores player hand if player also has blackjack.
+    -> Changed variable names to snake_case.
+## Bouncing DvD
+    -> Added f-strings.
+    -> Changed variable names to snake_case.
+    -> Fixed boudry logic
