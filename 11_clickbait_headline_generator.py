c8cb085d5c42c58fa163b4221c154887591b6081
Alex Flores <alexontheiphone@gmail.com>
2026-09-12 03:34:09 +0000

    WIP

---
+++ 11_clickbait_headline_generator.py
@@ -0,0 +1,9 @@
+"""Clickbait Headline Generator, by Alex Flores alexflores83 on X
+A clickbait headline generator for your soulless content farm website.
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: large, beginner, humor, word"""
+
+import random
+
+# Set up the constants:
+OBJECT_PRONOUNS = ["Her"]
