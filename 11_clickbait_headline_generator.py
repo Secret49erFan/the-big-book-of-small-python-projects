9806b9cd791f724e3ed03ca9e3b406d7d680f364
Alex Flores <alexontheiphone@gmail.com>
2026-09-25 05:36:03 +0000

    Add new headline generation functions to Clickbait Headline Generator

--- 11_clickbait_headline_generator.py
+++ 11_clickbait_headline_generator.py
@@ -68,6 +68,64 @@ def generate_are_millenials_killing_headline():
     noun = random.choice(NOUNS)
     return f"Are Millenials Killing the {noun} Industry?"
 
+
 def generate_what_you_dont_know_headline():
     noun = random.choice(NOUNS)
-    
+    plural_noun = random.choice(NOUNS) + "s"
+    when = random.choice(WHEN)
+    return f"Without this {noun}, {plural_noun} could kill you {when}"
+
+
+def generate_big_companies_hate_her_headline():
+    pronoun = random.choice(OBJECT_PRONOUNS)
+    state = random.choice(STATES)
+    noun1 = random.choice(NOUNS)
+    noun2 = random.choice(NOUNS)
+    return f"Big Companies Hate {pronoun}! See how this {state} {noun1} Invented a Cheaper {noun2}"
+
+
+def generate_you_wont_believe_headline():
+    state = random.choice(STATES)
+    noun = random.choice(NOUNS)
+    pronoun = random.choice(POSSISIVE_PRONOUNS)
+    place = random.choice(PLACES)
+    return f"You Won't Believe What This {state} {noun} Found in {pronoun} {place}"
+
+
+def generate_dont_want_you_know_headline():
+    plural_noun1 = random.choice(NOUNS) + "s"
+    plural_noun2 = random.choice(NOUNS) + "s"
+    return f"What {plural_noun1} Don't Want You To Know About {plural_noun2}"
+
+
+def generate_gift_idea_headline():
+    number = random.randin1000t(7, 15)
+    noun = random.choice(NOUNS)
+    state = random.choice(STATES)
+    return f"{number} Gift Ideas to Give Your {noun} From {state}"
+
+
+def generate_reasons_why_headline():
+    number1 = random.randint(3, 19)
+    plural_noun = random.choice(NOUNS) + "s"
+    # number2 should be no larger than number1:
+    number2 = random.randint(1, number1)
+    return f"{number1} Reason Why {plural_noun} Are More Interesting Than You Think (Number {number2} Will Surprise You!)"
+
+
+def generate_jobs_automated_headline():
+    state = random.choice(STATES)
+    noun = random.choice(NOUNS)
+
+    i = random.randint(0, 2)
+    pronoun1 = POSSISIVE_PRONOUNS[i]
+    pronoun2 = PERSONAL_PRONOUNS[i]
+    if pronoun1 == "Their":
+        return f"This {state} {noun} Didn't Think Robots Would Take {pronoun1}. {pronoun2} Were Wrong."
+    else:
+        return f"This {state} {noun} Didn't Think Robots Would Take {pronoun1}. {pronoun2} Was Wrong."
+
+
+# If the program is run (instead of imported), run the game:
+if __name__ == "__main__":
+    main()
